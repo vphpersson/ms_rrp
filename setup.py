01cf6fd75580119e4b6f01c469e995592c6049ca
Viktor Persson <vph.persson@gmail.com>
2020-01-15 23:20:29 +0100

    Initial.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='ms_rrp',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'rpc @ https://github.com/vphpersson/rpc/tarball/master',
+        'msdsalgs @ https://github.com/vphpersson/msdsalgs/tarball/master'
+    ]
+)
