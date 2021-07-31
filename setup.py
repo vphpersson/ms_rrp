9dd035e1d95b893840af22fe738d62305895f466
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2021-07-31 16:59:29 +0200

    Fix depenedencies; add `dump_reg` function.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     install_requires=[
         'rpc @ git+ssh://git@github.com/vphpersson/rpc.git#egg=rpc',
         'msdsalgs @ git+ssh://git@github.com/vphpersson/msdsalgs.git#egg=msdsalgs',
-        'msdsalgs @ git+ssh://git@github.com/vphpersson/ndr.git#egg=ndr'
-
+        'ndr @ git+ssh://git@github.com/vphpersson/ndr.git#egg=ndr',
+        'smb @ git+ssh://git@github.com/vphpersson/smb.git#egg=smb'
     ]
 )
