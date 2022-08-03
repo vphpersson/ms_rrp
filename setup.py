c6181962082c5f9be8c26a99d5803ef61f91d3fe
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2022-08-04 01:06:06 +0200

    Fix references.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 setup(
     name='ms_rrp',
-    version='0.1',
+    version='0.11',
     packages=find_packages(),
     install_requires=[
-        'rpc @ git+ssh://git@github.com/vphpersson/rpc.git#egg=rpc',
-        'msdsalgs @ git+ssh://git@github.com/vphpersson/msdsalgs.git#egg=msdsalgs',
-        'ndr @ git+ssh://git@github.com/vphpersson/ndr.git#egg=ndr',
-        'smb @ git+ssh://git@github.com/vphpersson/smb.git#egg=smb'
+        'rpc @ git+https://github.com/vphpersson/rpc.git#egg=rpc',
+        'msdsalgs @ git+https://github.com/vphpersson/msdsalgs.git#egg=msdsalgs',
+        'ndr @ git+https://github.com/vphpersson/ndr.git#egg=ndr',
+        'smb @ git+https://github.com/vphpersson/smb.git#egg=smb'
     ]
 )
